5203ab24708dfa67aa87f53b156b2da774ed4a91
Daronec <60248562+Daronec@users.noreply.github.com>
2021-08-06 12:48:40 +0300

    Update app_colors.dart

--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -8,6 +8,7 @@ class AppColors {
   static const background = Color(0xFF191919);
   static const background_panel = Color(0xFF323232);
   static const red = Color(0xFFff5a00);
+  static const orange = Color(0xFFffa500);
   static const yellow = Color(0xFFfdff00);
   static const light_yellow = Color(0xFFfeff8b);
   static const green = Color(0xFFA7CC7B);
